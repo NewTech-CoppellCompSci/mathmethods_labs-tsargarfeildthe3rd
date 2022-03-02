b2245a3c1c4052227f87ced73a7984d1bbb5d76b
rhett <Rhett.martin@newtech.coppellisd.com>
2022-03-02 14:35:02 -0600

    SDF

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,7 +1,7 @@
 package labs;
 
-
-
+import java.util.Scanner;
+import java.lang.Math;
 public class Lab01 {
 
 	public static void main(String[] args) {
@@ -17,8 +17,15 @@ public class Lab01 {
 	
 	public static void problem01() {
 		
+		Scanner joeMamma = new Scanner(System.in);
+		System.out.println("Enter a positive integer");
+		int joe = joeMamma.nextInt();
 		
+		System.out.println("Enter a positive integer");
+		int mama = joeMamma.nextInt();
 		
+		double soUgly = (int) Math.pow(joe,mama);
+		System.out.println(soUgly);
 	}
 	
 	
@@ -26,7 +33,12 @@ public class Lab01 {
 	
 	public static void problem02() {
 		
+		Scanner square = new Scanner(System.in);
+		System.out.println("Enter a positive integer");
+		int joe =square.nextInt();
+		double yourMom = Math.sqrt(joe);
 		
+		System.out.println("The square root of " + joe +"is" + yourMom);
 		
 	}
 
@@ -34,10 +46,18 @@ public class Lab01 {
 	
 
 	public static void problem03() {
+		Scanner whiteBeard = new Scanner(System.in);
+		System.out.println("Enter a positive integer");
+		int died =whiteBeard.nextInt();
 		
-		
-		
+		System.out.println("LboZo");
+		int inMarineford = whiteBeard.nextInt();
 
+		double so = died*died;
+		double does = inMarineford*inMarineford;
+		
+		double ace = so + does;
+		double lBozo = Math.sqrt(ace);
 	}
 
 
